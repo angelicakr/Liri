0fa7ba366f19ba727c49bf2f8c0d1abdbe49ffa0
Angelica Karina Rivas <angelicakarina.rivas@gmail.com>
2018-10-04 18:27:59 -0600

    Second Review

--- liri.js
+++ liri.js
@@ -1,128 +1,129 @@
 require("dotenv").config();
 var keys = require('./keys.js');
 var request = require('request');
-var bandsintown = require('bandsintown')
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-var omdb = require('omdb');
 var fs = require('fs');
 
 //argument to store elements
-var database = process.argv[2]; // search on API database
-var liriCommand = process.argv[3]; //search term
+var searchTerm = process.argv.slice(3).join(" "); // search on API database
+var liriCommand = process.argv[2]; //search term
 
 //commands for liri
-if (liriCommand === "spotify-this-song" || liriCommand === "concert-this") {
-    spotify();
-    concert();
 
-} if (liriCommand === "movie-this"){
-    movie();
+//Functions
+//var commands = {
+
+var song = function (song) {
+    spotify.search({
+        type: 'track',
+        query: song
+    }, function (error, data) {
+        if (!error) {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var songData = data.tracks.items[i];
+                //artist
+                console.log("Artist: " + songData.artists[0].name);
+                //song name
+                console.log("Song: " + songData.name);
+                //spotify preview link
+                console.log("Preview URL: " + songData.preview_url);
+                //album name
+                console.log("Album: " + songData.album.name);
+                console.log("-----------------------");
+
+                //adds text to log.txt
+                fs.appendFile('log.txt', songData.artists[0].name);
+                fs.appendFile('log.txt', songData.name);
+                fs.appendFile('log.txt', songData.preview_url);
+                fs.appendFile('log.txt', songData.album.name);
+                fs.appendFile('log.txt', "-----------------------");
+            }
+        } else {
+            console.log('Error occurred.');
+        }
+    });
+}
+var movie = function (movie) {
+    var omdbURL = 'http://www.omdbapi.com/?i=tt3896198&t=' + movie +'&apikey=c3af164f';
+
+    request(omdbURL, function (err, response, body) {
+        if (err) {
+            return console.error('Error occurred.');
+        } else {
+            var data = JSON.parse(body);
+        
+            console.log("Title: " + data.Title);
+            console.log("Release Year: " + data.Year);
+            console.log("IMdB Rating: " + data.imdbRating);
+            console.log("Country: " + data.Country);
+            console.log("Language: " + data.Language);
+            console.log("Plot: " + data.Plot);
+            console.log("Actors: " + data.Actors);
+            console.log("Rotten Tomatoes Rating: " + data.Ratings[1].Value);
+            
+
+            //adds text to log.txt
+            //fs.appendFile('log.txt', "Title: " + data.Title);
+            //fs.appendFile('log.txt', "Release Year: " +  data.Year);
+            //fs.appendFile('log.txt', "IMdB Rating: " +  data.imdbRating);
+            //fs.appendFile('log.txt', "Country: " +  data.Country);
+            //fs.appendFile('log.txt', "Language: " +  data.Language);
+            //fs.appendFile('log.txt', "Plot: " +  data.Plot);
+            //fs.appendFile('log.txt', "Actors: " +  data.Actors);
+            //fs.appendFile('log.txt', "Rotten Tomatoes Rating: " +  data.Ratings[1].Value);
+        
+
+
+        }
+    });
+}
+var concert = function (artist) {
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    request(queryURL, function (error, response, body) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            console.log("\nBand search: " + artist);
+            var body = JSON.parse(body);
+
+            for (var i = 0; i < body.length; i++) {
+
+                var find = body[i].venue;
+                var datetime = body[i].datetime;
+
+
+                var bands = [
+                    "Venue: " + find.name,
+                    "Location: " + find.city + ", " + find.region,
+                    "Date: " + datetime,
+                    // "Time: " + time,
+                ].join("\n");
+                console.log("\nEvent " + (i + 1) + ": " + "\nVenue: " + bands);
+
+                //fs.appendFile("log.txt", "\nConcert search...\nBand: " + artist + "\nEvent " + (i + 1) + ": " + "\nVenue: " + bands + divider, function (err) {
+                   // if (err) throw err;
+                //});
+
+            }
+
+            // Write the object to the command line
+        } else {
+            console.log("Something went wrong.");
+        }
+    });
+}
+// liri commands
+if (liriCommand === "spotify-this-song") {
+    song(searchTerm);
+} else if (liriCommand === "concert-this"){
+    concert(searchTerm);
+} else if (liriCommand === "movie-this") {
+    movie(searchTerm);
 
 } else {
     console.log("error")
-}
-//Functions
-var commands = {
-
-    spotify: function (song) {
-        spotify.search({
-            type: 'track',
-            query: song
-        }, function (error, data) {
-            if (!error) {
-                for (var i = 0; i < data.tracks.items.length; i++) {
-                    var songData = data.tracks.items[i];
-                    //artist
-                    console.log("Artist: " + songData.artists[0].name);
-                    //song name
-                    console.log("Song: " + songData.name);
-                    //spotify preview link
-                    console.log("Preview URL: " + songData.preview_url);
-                    //album name
-                    console.log("Album: " + songData.album.name);
-                    console.log("-----------------------");
-
-                    //adds text to log.txt
-                    fs.appendFile('log.txt', songData.artists[0].name);
-                    fs.appendFile('log.txt', songData.name);
-                    fs.appendFile('log.txt', songData.preview_url);
-                    fs.appendFile('log.txt', songData.album.name);
-                    fs.appendFile('log.txt', "-----------------------");
-                }
-            } else {
-                console.log('Error occurred.');
-            }
-        });
-    },
-    movie: function (movie) {
-        var omdbURL = 'http://www.omdbapi.com/?i=tt3896198&apikey=c3af164f';
-
-        omdb.search(omdbURL, function (err, movies) {
-            if (err) {
-                return console.error('Error occurred.');
-            } else {
-                console.log("Title: " + body.Title);
-                console.log("Release Year: " + body.Year);
-                console.log("IMdB Rating: " + body.imdbRating);
-                console.log("Country: " + body.Country);
-                console.log("Language: " + body.Language);
-                console.log("Plot: " + body.Plot);
-                console.log("Actors: " + body.Actors);
-                console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-                console.log("Rotten Tomatoes URL: " + body.tomatoURL);
-
-                //adds text to log.txt
-                fs.appendFile('log.txt', "Title: " + body.Title);
-                fs.appendFile('log.txt', "Release Year: " + body.Year);
-                fs.appendFile('log.txt', "IMdB Rating: " + body.imdbRating);
-                fs.appendFile('log.txt', "Country: " + body.Country);
-                fs.appendFile('log.txt', "Language: " + body.Language);
-                fs.appendFile('log.txt', "Plot: " + body.Plot);
-                fs.appendFile('log.txt', "Actors: " + body.Actors);
-                fs.appendFile('log.txt', "Rotten Tomatoes Rating: " + body.tomatoRating);
-                fs.appendFile('log.txt', "Rotten Tomatoes URL: " + body.tomatoURL);
-
-
-            }
-        });
-    },
-    concert: function () {
-
-
-        request(queryURL, function (error, response, body) {
-
-            // If the request is successful
-            if (!error && response.statusCode === 200) {
-                console.log("\nBand search: " + band);
-                var body = JSON.parse(body);
-
-                for (var i = 0; i < body.length; i++) {
-
-                    var find = body[i].venue;
-                    var datetime = body[i].datetime;
-
-
-                    var bands = [
-                        "Venue: " + find.name,
-                        "Location: " + find.city + ", " + find.region,
-                        "Date: " + date,
-                        // "Time: " + time,
-                    ].join("\n");
-                    console.log("\nEvent " + (i + 1) + ": " + "\nVenue: " + bands);
-
-                    fs.appendFile("log.txt", "\nConcert search...\nBand: " + band + "\nEvent " + (i + 1) + ": " + "\nVenue: " + bands + divider, function (err) {
-                        if (err) throw err;
-                    });
-
-                }
-
-                // Write the object to the command line
-            } else {
-                console.log("Something went wrong.");
-            }
-        });
-    }
-
 
 }
